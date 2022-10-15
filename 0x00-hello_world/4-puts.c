039b2674589556e6de8afe5f9e5492521cd62d0e
Kalu Goodness <goodkc12@gmail.com>
2022-10-15 13:25:07 -0700

    First

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *
+ *  *  *main- Serves as the entry point for the program
+ *
+ *   *   *
+ *
+ *    *    *Return: By default returns zero if no erros non-zero value.
+ *
+ *     *
+ *
+ *      */
+int main(void)
+{
+			puts("\"Programming is like building a multilingual puzzle");
+						return (0);
+}
