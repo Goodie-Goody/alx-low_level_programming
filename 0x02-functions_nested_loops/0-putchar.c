fbd175448cd8ba86f3e669001ad516e8e04cea6e
Kalu Goodness <goodkc12@gmail.com>
2022-10-18 13:35:09 -0700

    zero

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,22 @@
+#include "main.h"
+/**
+ * main - ENtry point
+ * Authored by Kalu Goodness Chibueze
+ * Return: ALways 0 (Success)
+ */
 
+int main(void)
+{
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
